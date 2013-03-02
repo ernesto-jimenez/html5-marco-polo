91b1fc4cda0793d48d34ce93a194a0d1ffe193c7
Ernesto Jiménez <me@ernesto-jimenez.com>
2013-03-02 17:49:12 +0000

    basic logic for the game finalised

--- js/app.js
+++ js/app.js
@@ -2,19 +2,28 @@ function Game(container) {
   var scope = this;
   scope.visible = true;
   scope.translateCoords = true;
-  scope.playSound = false;
+  scope.playSound = true;
   scope.playing = false;
   scope.container = container;
   scope.direction = 0;
   scope.keys = {
     38: 'forward',
-    40: 'backward',
+    41: 'backward',
     39: 'right',
     37: 'left'
   };
   scope.gameplay = new Gameplay();
   scope.sound = new SoundPlayer(new webkitAudioContext());
 
+  this.testFront = function () {
+    scope.gameplay.player.position.set(100, 200);
+    scope.gameplay.player.direction = 90;
+  };
+
+  this.testBack = function () {
+    scope.gameplay.player.position.set(100, 200);
+    scope.gameplay.player.direction = 270;
+  };
 
   this.start = function () {
     if (!scope.canvas) {
@@ -51,7 +60,7 @@ function Game(container) {
 
     scope.canvas.width = scope.width;
     scope.canvas.height = scope.height;
-    scope.center = new THREE.Vector2(scope.width / 2, scope.height / 2);
+    scope.center = new THREE.Vector3(scope.width / 2, scope.height / 2, 0);
     scope.context = scope.canvas.getContext('2d');
   }
 
@@ -73,7 +82,7 @@ function Game(container) {
     if(scope.visible) { scope.draw(); }
 
     if(scope.playSound) {
-      scope.sound.changePosition(scope.gameplay.relativePosition());
+      scope.sound.changePosition(scope.gameplay);
     } else {
       scope.sound.changePosition();
     }
@@ -92,20 +101,18 @@ function Game(container) {
 
   this.updateTranslation = function () {
     scope.playing = true;
-    scope.translation = scope.translation || new THREE.Vector2();
+    scope.translation = scope.translation || new THREE.Vector3();
     scope.translation.subVectors(scope.center, scope.gameplay.player.position);
   };
 
   this.worldToCam = function (v) {
-    //return v;
     return scope.translateCoords ? scope.translation.clone().add(v) : v;
   };
 
   this.drawHud = function (ctx) {
-    var orig = scope.worldToCam(new THREE.Vector2(0, 0)),
-    axeX = scope.worldToCam(new THREE.Vector2(100, 0)),
-    axeY = scope.worldToCam(new THREE.Vector2(0, 100));
-    ctx.save();
+    var orig = scope.worldToCam(new THREE.Vector3(0, 0, 0)),
+    axeX = scope.worldToCam(new THREE.Vector3(100, 0, 0)),
+    axeY = scope.worldToCam(new THREE.Vector3(0, 100, 0));
     ctx.beginPath();
     ctx.strokeStyle = 'red';
     ctx.moveTo(orig.x, orig.y);
@@ -123,51 +130,45 @@ function Game(container) {
         pos = scope.worldToCam(gameplay.player.position);
 
     ctx.beginPath();
-    //ctx.translate(
-      //Math.floor(scope.canvas.width / 2),
-      //Math.floor(scope.canvas.height / 2));
-    //ctx.rotate((scope.gameplay.player.direction + 90) * (Math.PI / 180));
-    dir = scope.worldToCam(gameplay.deltaVector(1).setLength(-10).add(gameplay.player.position));
+    dir = scope.worldToCam(gameplay.facingVector().add(gameplay.player.position));
 
     ctx.fillStyle = "white";
 
     ctx.arc(pos.x, pos.y, 10, 0, Math.PI*2, false);
     ctx.stroke();
     ctx.fill();
+
+    // Delta vector
     ctx.moveTo(pos.x, pos.y);
     ctx.lineTo(dir.x, dir.y);
     ctx.stroke();
-    ctx.restore();
   };
 
   this.drawObjective = function (ctx) {
     var gameplay = scope.gameplay,
         pos = scope.worldToCam(gameplay.objective.position);
-        //pos = gameplay.relativePosition();
 
-    ctx.save();
     ctx.beginPath();
-    //ctx.translate(
-      //Math.floor(scope.canvas.width / 2),
-      //Math.floor(scope.canvas.height / 2));
-    //ctx.rotate((gameplay.player.direction - 270) % 270 * (Math.PI / 180));
 
+    ctx.strokeStyle = 'black';
     ctx.arc(pos.x, pos.y, 10, 0, Math.PI*2, false);
     ctx.stroke();
 
-    ctx.restore();
   };
 
   this.draw = function () {
     var ctx = scope.context;
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, scope.width, scope.height);
+    ctx.save();
     scope.drawHud(ctx);
     scope.drawObjective(ctx);
+    ctx.restore();
   };
 }
 
 function SoundPlayer(context) {
+    // http://freesound.org/people/Tewkesound/sounds/140147/
     var urls = ['position.wav'];
     var scope = this;
     this.isPlaying = false;
@@ -178,20 +179,21 @@ function SoundPlayer(context) {
 
     this.play = function () {
       var source = context.createBufferSource();
+      if (!this.buffer) return false;
       source.buffer = this.buffer;
       source.loop = true;
       var panner = context.createPanner();
       //panner.coneOuterGain = 0.1;
       //panner.coneOuterAngle = 180;
-      //panner.coneInnerAngle = 0;
-      panner.rolloffFactor = 0.1;
+      //panner.coneInnerAngle = 20;
+      panner.rolloffFactor = 0.05;
       panner.connect(context.destination);
       source.connect(panner);
       source.noteOn(0);
-      context.listener.setPosition(0, 0, 0);
       scope.source = source;
       scope.panner = panner;
       scope.isPlaying = true;
+      return true;
     };
 
     this.stop = function () {
@@ -199,15 +201,18 @@ function SoundPlayer(context) {
       scope.isPlaying = false;
     };
 
-    this.changePosition = function (position) {
-      if (position) {
-        if (!scope.isPlaying) {
-          scope.play();
+    this.changePosition = function (gameplay) {
+      if (gameplay) {
+        if (!scope.isPlaying && !scope.play()) {
+          return;
         }
-        var mul = 1;
-        var x = position.x;
-        var y = -position.y;
-        scope.panner.setPosition(x * mul, y * mul, 0);
+        var player = gameplay.player.position,
+            objective = gameplay.objective.position,
+            orient = gameplay.facingVector().negate();
+
+        context.listener.setOrientation(orient.x, orient.y, 0, 0, 0, 1);
+        context.listener.setPosition(player.x, player.y, 0);
+        scope.panner.setPosition(objective.x, objective.y, 0);
       } else {
         scope.stop();
       }
@@ -216,25 +221,27 @@ function SoundPlayer(context) {
 
 function Gameplay() {
   var scope = this;
+  var quaternion = new THREE.Quaternion();
+  var rotationAxis = new THREE.Vector3(0,0,1);
 
   this.speed = 100;
   this.turnSpeed = 180;
-  this.relativePos = new THREE.Vector2();
+  this.relativePos = new THREE.Vector3();
 
   this.player = {
-    position: new THREE.Vector2(),
+    position: new THREE.Vector3(),
     direction: 270
   };
 
   this.objective = {
-    position: new THREE.Vector2(),
+    position: new THREE.Vector3(),
     direction: 0
   };
 
   this.start = function () {
-    this.player.position.set(200, 200);
-    this.player.direction = 270;
-    this.objective.position.set(100, 100);
+    this.player.position.set(200, 200, 0);
+    this.player.direction = 90;
+    this.objective.position.set(100, 100, 0);
     this.objective.direction = 0;
   };
 
@@ -242,12 +249,25 @@ function Gameplay() {
     return scope.relativePos.subVectors(scope.objective.position, scope.player.position);
   };
 
+  this.facingVector = function () {
+    return scope.deltaVector(1).setLength(-10);
+  };
+
+  this.soundVector = function () {
+    quaternion.setFromAxisAngle(rotationAxis, (scope.player.direction + 90) * (Math.PI / 180));
+    return scope.relativePosition().clone().applyQuaternion(quaternion);
+  };
+
   this.turnRight = function (delta) {
-    scope.player.direction -= scope.turnSpeed * delta;
+    var player = scope.player;
+    player.direction -= scope.turnSpeed * delta;
+    player.direction = player.direction % 360;
   };
 
   this.turnLeft = function (delta) {
-    scope.player.direction += scope.turnSpeed * delta;
+    var player = scope.player;
+    player.direction += scope.turnSpeed * delta;
+    player.direction = player.direction % 360;
   };
 
   this.deltaVector = function (delta) {
@@ -257,7 +277,7 @@ function Gameplay() {
     deltaY = Math.sin(theta * (Math.PI / 180)) * distance;
 
     //console.log({angle: theta, deltaX: deltaX, deltaY: deltaY});
-    return new THREE.Vector2(deltaX, deltaY);
+    return new THREE.Vector3(deltaX, deltaY);
   };
 
   this.moveForward = function (delta) {
@@ -269,6 +289,7 @@ function Gameplay() {
   };
 
   this.winsGame = function () {
+    //console.log(scope.relativePosition().length());
     return scope.relativePosition().length() < 20;
   };
 }
@@ -277,6 +298,8 @@ window.onload = function () {
   game = new Game(document.body);
   var gui = new dat.GUI();
   gui.add(game, 'start');
+  gui.add(game, 'testFront');
+  gui.add(game, 'testBack');
   gui.add(game, 'visible');
   gui.add(game, 'playSound');
   gui.add(game, 'translateCoords');
